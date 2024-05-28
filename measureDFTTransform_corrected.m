ed2539854ad1ac038ec815950d16acadcdc98ea9
AgentDoodzik <115586234+AgentDoodzik@users.noreply.github.com>
2024-05-28 23:21:24 +0200

    Update the code to draw Bode plots for all three amplitudes of input signal
    TODO:
    Add comments in places requiring more in-depth explaination
    Add proper main titles to bode plots

--- measureDFTTransform_corrected.m
+++ measureDFTTransform_corrected.m
@@ -35,56 +35,69 @@ filePaths = {
 % transposed for convenience
 % first row - input (sine wave)
 % second row - output
-A1__0_1Hz = table2array(readSelectedColumns(filePaths{1}))';
-A1__0_125Hz = table2array(readSelectedColumns(filePaths{2}))';
-A1__0_167Hz = table2array(readSelectedColumns(filePaths{3}))';
-A1__0_25Hz = table2array(readSelectedColumns(filePaths{4}))';
-A1__0_5Hz = table2array(readSelectedColumns(filePaths{5}))';
-A1__1Hz = table2array(readSelectedColumns(filePaths{6}))';
-A1__2Hz = table2array(readSelectedColumns(filePaths{7}))';
-A1__4Hz = table2array(readSelectedColumns(filePaths{8}))';
+A1_sineFreq1 = table2array(readSelectedColumns(filePaths{1}))';
+A1_sineFreq2 = table2array(readSelectedColumns(filePaths{2}))';
+A1_sineFreq3 = table2array(readSelectedColumns(filePaths{3}))';
+A1_sineFreq4 = table2array(readSelectedColumns(filePaths{4}))';
+A1_sineFreq5 = table2array(readSelectedColumns(filePaths{5}))';
+A1_sineFreq6 = table2array(readSelectedColumns(filePaths{6}))';
+A1_sineFreq7 = table2array(readSelectedColumns(filePaths{7}))';
+A1_sineFreq8 = table2array(readSelectedColumns(filePaths{8}))';
 
-% A0_75__0_1Hz = table2array(readSelectedColumns(filePaths{9}))';
-% A0_75__0_125Hz = table2array(readSelectedColumns(filePaths{10}))';
-% A0_75__0_167Hz = table2array(readSelectedColumns(filePaths{11}))';
-% A0_75__0_25Hz = table2array(readSelectedColumns(filePaths{12}))';
-% A0_75__0_5Hz = table2array(readSelectedColumns(filePaths{13}))';
-% A0_75__1Hz = table2array(readSelectedColumns(filePaths{14}))';
-% A0_75__2Hz = table2array(readSelectedColumns(filePaths{15}))';
-% A0_75__4Hz = table2array(readSelectedColumns(filePaths{16}))';
-% 
-% A0_5__0_1Hz = table2array(readSelectedColumns(filePaths{17}))';
-% A0_5__0_125Hz = table2array(readSelectedColumns(filePaths{18}))';
-% A0_5__0_167Hz = table2array(readSelectedColumns(filePaths{19}))';
-% A0_5__0_25Hz = table2array(readSelectedColumns(filePaths{20}))';
-% A0_5__0_5Hz = table2array(readSelectedColumns(filePaths{21}))';
-% A0_5__1Hz = table2array(readSelectedColumns(filePaths{22}))';
-% A0_5__2Hz = table2array(readSelectedColumns(filePaths{23}))';
-% A0_5__4Hz = table2array(readSelectedColumns(filePaths{24}))';
+A0_75_sineFreq1 = table2array(readSelectedColumns(filePaths{9}))'; % corrupt measurement
+A0_75_sineFreq2 = table2array(readSelectedColumns(filePaths{10}))';
+A0_75_sineFreq3 = table2array(readSelectedColumns(filePaths{11}))';
+A0_75_sineFreq4 = table2array(readSelectedColumns(filePaths{12}))';
+A0_75_sineFreq5 = table2array(readSelectedColumns(filePaths{13}))';
+A0_75_sineFreq6 = table2array(readSelectedColumns(filePaths{14}))';
+A0_75_sineFreq7 = table2array(readSelectedColumns(filePaths{15}))';
+A0_75_sineFreq8 = table2array(readSelectedColumns(filePaths{16}))';
+
+A0_5_sineFreq1 = table2array(readSelectedColumns(filePaths{17}))';
+A0_5_sineFreq2 = table2array(readSelectedColumns(filePaths{18}))'; 
+A0_5_sineFreq3 = table2array(readSelectedColumns(filePaths{19}))';
+A0_5_sineFreq4 = table2array(readSelectedColumns(filePaths{20}))';
+A0_5_sineFreq5 = table2array(readSelectedColumns(filePaths{21}))';
+A0_5_sineFreq6 = table2array(readSelectedColumns(filePaths{22}))';
+A0_5_sineFreq7 = table2array(readSelectedColumns(filePaths{23}))';
+A0_5_sineFreq8 = table2array(readSelectedColumns(filePaths{24}))';
 
 sineFrequencies = [0.0960, 0.1330, 0.1664, 0.2470, 0.5004, 1.0164, 2.0151, 4.0099]; %Hz, slightly differs from initially accepted test frequencies
 
-matrixOfResults={A1__0_1Hz, A1__0_125Hz, A1__0_167Hz, A1__0_25Hz, A1__0_5Hz, A1__1Hz, A1__2Hz, A1__4Hz} %, A0_75__0_1Hz, A0_75__0_125Hz, A0_75__0_167Hz, A0_75__0_25Hz, A0_75__0_5Hz, A0_75__1Hz, A0_75__2Hz, A0_75__4Hz, A0_5__0_1Hz, A0_5__0_125Hz, A0_5__0_167Hz, A0_5__0_25Hz, A0_5__0_5Hz, A0_5__1Hz, A0_5__2Hz, A0_5__4Hz};
+matrixOfResults={A1_sineFreq1, A1_sineFreq2, A1_sineFreq3, A1_sineFreq4, ...
+    A1_sineFreq5, A1_sineFreq6, A1_sineFreq7, A1_sineFreq8, ...
+    A0_75_sineFreq1, A0_75_sineFreq2, A0_75_sineFreq3, A0_75_sineFreq4, A0_75_sineFreq5,...
+    A0_75_sineFreq6, A0_75_sineFreq7, A0_75_sineFreq8,...
+    A0_5_sineFreq1, A0_5_sineFreq2, A0_5_sineFreq3, A0_5_sineFreq4,...
+    A0_5_sineFreq5, A0_5_sineFreq6, A0_5_sineFreq7, A0_5_sineFreq8 } 
+%AmplitudeSineFreqInfo = {'A = 1, f_{sin} = 0.0960Hz', 'A = 1, f_{sin} = 0.1330Hz', 'A = 1, f_{sin} = 0.1664Hz', 'A = 1, f_{sin} = 0.2470Hz', 'A = 1, f_{sin} = 0.5004Hz', 'A = 1, f_{sin} = 1.0164Hz', 'A = 1, f_{sin} = 2.0151Hz', 'A = 1, f_{sin} = 4.0099Hz'};
 
-AmplitudeSineFreqInfo = {'A = 1, f_{sin} = 0.0960Hz', 'A = 1, f_{sin} = 0.1330Hz', 'A = 1, f_{sin} = 0.1664Hz', 'A = 1, f_{sin} = 0.2470Hz', 'A = 1, f_{sin} = 0.5004Hz', 'A = 1, f_{sin} = 1.0164Hz', 'A = 1, f_{sin} = 2.0151Hz', 'A = 1, f_{sin} = 4.0099Hz'};
+% A0_75_sineFreq1 - corrupt - might need skipping
 
 %%
-iter = 0;
+iter = 1;
+rowSelector1of3 = 1;
+rowSelector1of6 = 1;
+freqSelector = 1;
+columnSelector=1
 
-plot_iterator = 1;
+% six row table
+% first and second row - Magnitude and Phase values, measurement with
+% Amplitude = 1.0
+% third and fourth - Magnitude and phase, meas with A = 0.75
+% 5th and 6th - similar as above
+MagnitudePhaseInput = zeros(6,length(sineFrequencies)); %data from input signal 
+MagnitudePhaseOutput = zeros(6,length(sineFrequencies)); %from output signal
 
-% two-row table - first row: A (complex num. magnitude), second: angle 
-MagnitudePhaseInput = zeros(2,length(sineFrequencies)); %data from input signal 
-MagnitudePhaseOutput = zeros(2,length(sineFrequencies)); %from output signal
-Gs = zeros(1, length(sineFrequencies)); % gain
-fis = zeros(1, length(sineFrequencies)); % phase
+Gs = zeros(3, length(sineFrequencies)); % gain, 1st row - A = 1.0; 2nd - A = 0.75; 3rd A = 0.5 
+fis = zeros(3, length(sineFrequencies)); % phase, rows set as above
 
 
-ReWe=zeros(1,length(sineFrequencies)); %real part of input signal
-ImWe=zeros(1,length(sineFrequencies)); %Imaginary part of output signal
+ReWe=zeros(3,length(sineFrequencies)); %real part of input signal
+ImWe=zeros(3,length(sineFrequencies)); %Imaginary part of output signal
 
-ReWy=zeros(1,length(sineFrequencies));
-ImWy=zeros(1,length(sineFrequencies));
+ReWy=zeros(3,length(sineFrequencies));
+ImWy=zeros(3,length(sineFrequencies));
 
 for iter=1:length(matrixOfResults);
 
@@ -100,25 +113,16 @@ t=(0:Nt-1)/fp; %time vector needed for DFT
 
 Nf = round(Nt/2+1); %sample corresponding to Nyquist frequency
 
+freq=sineFrequencies(freqSelector); %calculating DFT for a specific test freq.
 
+ReWe(rowSelector1of3,columnSelector) = 2/Nt * korL(matrixOfResults{iter}(1,:), cos(2*pi*freq*t)); % real part DFT 
+ImWe(rowSelector1of3,columnSelector) = 2/Nt * korL(matrixOfResults{iter}(1,:), sin(2*pi*freq*t)); %imaginary part DFT
 
-
-
-% for fi=1:length(sineFrequencies)
-
-    freq=sineFrequencies(iter); %calculating DFT for a specific test freq.
-
-    ReWe(iter) = 2/Nt * korL(matrixOfResults{iter}(1,:), cos(2*pi*freq*t)); % real part DFT 
-    ImWe(iter) = 2/Nt * korL(matrixOfResults{iter}(1,:), sin(2*pi*freq*t)); %imaginary part DFT
-
-    ReWy(iter) = 2/Nt * korL(matrixOfResults{iter}(2,:), cos(2*pi*freq*t));
-    ImWy(iter) = 2/Nt * korL(matrixOfResults{iter}(2,:), sin(2*pi*freq*t));
+ReWy(rowSelector1of3,columnSelector) = 2/Nt * korL(matrixOfResults{iter}(2,:), cos(2*pi*freq*t));
+ImWy(rowSelector1of3,columnSelector) = 2/Nt * korL(matrixOfResults{iter}(2,:), sin(2*pi*freq*t));
     
-% end
 
-
-
-% normalization 
+% normalization ?
 % ReWe(1) = ReWe(1)/2;
 % ReWe(Nf) = ReWe(Nf)/2;
 % 
@@ -131,91 +135,54 @@ Nf = round(Nt/2+1); %sample corresponding to Nyquist frequency
 % ImWy(1) = ImWy(1)/2;
 % ImWy(Nf) = ImWy(Nf)/2;
 
-MagnitudePhaseInput(1,iter)=sqrt(ReWe(iter)^2 + ImWe(iter)^2);
-MagnitudePhaseInput(2, iter)= atan2(ImWe(iter),ReWe(iter));
+MagnitudePhaseInput(rowSelector1of6,columnSelector)=sqrt(ReWe(rowSelector1of3,columnSelector)^2 + ImWe(rowSelector1of3,columnSelector)^2);
+MagnitudePhaseInput(rowSelector1of6+1,columnSelector)= atan2(ImWe(rowSelector1of3, columnSelector),ReWe(rowSelector1of3, columnSelector));
 
-MagnitudePhaseOutput(1,iter)=sqrt(ReWy(iter)^2 + ImWy(iter)^2);
-MagnitudePhaseOutput(2,iter)= atan2(ImWy(iter),ReWy(iter));
+MagnitudePhaseOutput(rowSelector1of6,columnSelector)=sqrt(ReWy(rowSelector1of3, columnSelector)^2 + ImWy(rowSelector1of3, columnSelector)^2);
+MagnitudePhaseOutput(rowSelector1of6+1,columnSelector)= atan2(ImWy(rowSelector1of3, columnSelector),ReWy(rowSelector1of3, columnSelector));
 
+if (iter==8) || (iter == 16)
+rowSelector1of6 = rowSelector1of6+2;
+rowSelector1of3 = rowSelector1of3+1;
+end
 
+if(freqSelector ~= 8)
+    freqSelector = freqSelector+1;
+    columnSelector = columnSelector+1;
+else
+    columnSelector = 1;
+    freqSelector = 1;
+end
 
-% figure(plot_iterator);
-% t=tiledlayout(2,1);
-% 
-% nexttile
-% plot(f, MagnitudePhaseInput{iter,1},'r-');
-% xlim([0 5]);
-% title("Ch-ka Bode'ego syg. we A(f)");
-% 
-% 
-% nexttile
-% plot(f, rad2deg(MagnitudePhaseInput{iter,2}),'b-');
-% xlim([0 5]);
-% title("Ch-ka Bode'ego syg. we fi(f)");
-% title(t,AmplitudeSineFreqInfo{iter});
-% plot_iterator = plot_iterator+1;
-% 
-% figure(plot_iterator);
-% t=tiledlayout(2,1);
-% 
-% nexttile
-% plot(f, MagnitudePhaseOutput{iter,1},'r-');
-% xlim([0 5]);
-% title("Ch-ka Bode'ego syg. wy A(f)");
-% 
-% nexttile
-% plot(f, rad2deg(MagnitudePhaseOutput{iter,2}),'b-');
-% xlim([0 5]);
-% title("Ch-ka Bode'ego syg. wy fi(f)");
-% title(t,AmplitudeSineFreqInfo{iter})
-% 
-% plot_iterator = plot_iterator+1;
-% figure(plot_iterator);
-% 
-% t=tiledlayout(2,1);
-% nexttile
-% plot(f, Gs{iter,1},'b-');
-% xlim([0 5]);
-% title("Ch-ka Bode'ego G(f)")
-% 
-% nexttile
-% plot(f,fis{iter,1},'-b');
-% xlim([0 5]);
-% title("Ch-ka Bode'ego dfi(f)")
-% title(t,AmplitudeSineFreqInfo{iter})
-% 
-% plot_iterator = plot_iterator+1;
+end
+
+Gs = MagnitudePhaseOutput(1:2:5,:) ./ MagnitudePhaseInput(1:2:5,:);
+fis = MagnitudePhaseOutput(2:2:6,:) - MagnitudePhaseInput(2:2:6,:); 
 
 
-
-
-
-end 
-
-Gs = MagnitudePhaseOutput(1,:) ./ MagnitudePhaseInput(1,:);
-fis = MagnitudePhaseOutput(2,:) - MagnitudePhaseInput(2,:); 
-
-
-
-    figure;
+for plotIter=1:size(Gs,1)
+    
+    figure(plotIter);
+    
     t = tiledlayout(2,1);
+    title('A = 1.0');
     
     nexttile
-    %plot(sineFrequencies,Gs,'b-');
-    stem(sineFrequencies,Gs);
-    xlim([0 4.5]);
+    plot(sineFrequencies,Gs(plotIter,:),'b-');
+    %stem(sineFrequencies,Gs(plotIter,:));
+    xlim([0 4.25]);
     title("Ch-ka Bode'ego G(f)");
     xlabel('Hz')
     ylabel('G')
     
     nexttile
-    %plot(sineFrequencies,rad2deg(fis));
-    stem(sineFrequencies,rad2deg(fis));
-    xlim([0 4.5]);
+    plot(sineFrequencies,rad2deg(fis(plotIter,:)));
+    %stem(sineFrequencies,rad2deg(fis(plotIter,:)));
+    xlim([0 4.25]);
     title("Ch-ka Bode'ego dfi(f)");
     xlabel('Hz')
     ylabel('delta fi')
     %title(t, AmplitudeSineFreqInfo);
 
-
+end
 
